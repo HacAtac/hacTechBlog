90a12f1ccc29de8dc843d6b43686254c70b3a044
HacAtac <jhack00@icloud.com>
2021-11-10 21:04:16 -0600

    refactor post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,25 +2,14 @@
 const sequelize = require("../../config/connection");
 const router = require("express").Router();
 //const sequelize = require("../../config/connection");
-const { isContext } = require("vm"); //this is a built in node module that is used to check if the object is empty
+//const { isContext } = require("vm"); //this is a built in node module that is used to check if the object is empty
 const { Post, User, Vote, Comment } = require("../../models"); // import the models
+const withAuth = require("../../utils/auth"); // import the auth middleware
 
 // get all users
 router.get("/", (req, res) => {
   Post.findAll({
-    attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      "post_content",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
-    ],
+    attributes: ["id", "post_text", "title", "created_at"],
     order: [["created_at", "DESC"]],
     include: [
       {
@@ -50,18 +39,7 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
-    ],
+    attributes: ["id", "post_text", "title", "created_at"],
     include: [
       {
         model: Comment,
@@ -90,53 +68,28 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// create post route with title req.body.title post_content req.body.post_content post_url req.body.post_url user_id req.body.user_id
-router.post("/", (req, res) => {
+// POST api/posts -- create a new post
+router.post("/", withAuth, (req, res) => {
+  // expects object of the form {title: 'Sample Title Here', post_text: 'Here's some sample text for a post.', user_id: 1}
   Post.create({
     title: req.body.title,
-    post_url: req.body.post_url,
-    post_content: req.body.post_content,
+    post_text: req.body.post_text,
     user_id: req.session.user_id,
   })
-    .then((dbPostData) => {
-      res.json(dbPostData);
-    })
+    .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-// put route to update post /api/posts/upvote
-router.put("/upvote", (req, res) => {
-  if (req.session) {
-    // this is a sequelize method that is used to update the vote count for a post
-    Post.upvote(
-      { ...req.body, user_id: req.session.user_id },
-      { Vote, Comment, User }
-    )
-      .then((updatedVoteData) => res.json(updatedVoteData))
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  }
-});
-
-//may need to change this route
-//put route to change title and body of a Post model
-router.put("/:id", (req, res) => {
-  Post.update(
-    {
-      title: req.body.title,
-      post_content: req.body.post_content,
+// put route to update post title or text
+router.put("/:id", withAuth, (req, res) => {
+  Post.update(req.body, {
+    where: {
+      id: req.params.id,
     },
-    {
-      where: {
-        id: req.params.id,
-      },
-    }
-  )
+  })
     .then((dbPostData) => {
       if (!dbPostData) {
         res.json(404).json({ message: "No post found with this id" });
@@ -151,13 +104,17 @@ router.put("/:id", (req, res) => {
 });
 
 //route to delete a post by id
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id,
     },
   })
     .then((dbPostData) => {
+      if (!dbPostData) {
+        res.json(404).json({ message: "No post found with this id" });
+        return;
+      }
       res.json(dbPostData);
     })
     .catch((err) => {
